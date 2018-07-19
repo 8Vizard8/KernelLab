91fec91a9668843e13f33549f85f9ce6716a586d
8Vizard8 <8Vizard8@users.noreply.github.com>
2018-07-19 23:48:56 +0300

    kernel-201 added

--- kernel.asm
+++ kernel.asm
@@ -5,13 +5,38 @@ section .text
   dd 0x1BADB002
   dd 0x00
   dd - (0x1BADB002)
+
 global start
+global keyboard_handler
+global read_port
+global write_port
+global load_idt
+
 extern kmain
+extern keyboard_handler_main
+
 start:
   cli
   mov esp, stack_space
   call kmain
   hlt
+read_port:
+  mov edx, dword[esp + 4]
+  in al, dx
+  ret
+write_port:
+  mov edx, dword[esp + 4]
+  mov al, byte[esp + 8]
+  out dx, al
+  ret
+load_idt:
+  mov edx, dword[esp+4]
+  lidt [edx]
+  sti
+  ret
+keyboard_handler:
+  call keyboard_handler_main
+  iretd
 section .bss
 resb 8192
 stack_space:
--- kernel.c
+++ kernel.c
@@ -1,7 +1,17 @@
+#include "keyboard_map.h"
+
+extern void keyboard_handler(void);
+extern char read_port(unsigned short port);
+extern void write_port(unsigned short port, unsigned char data);
+extern void load_idt(unsigned long *idt_ptr);
+extern unsigned char keyboard_map[128];
+
+char *vidptr = (char *)0xb8000;
+unsigned int current_loc = 0;
+
 void kmain(void) 
 {
   const char *str = "kernel by vz";
-  char *vidptr = (char *)0xb8000;
   unsigned int i = 0;
   unsigned int j = 0;
   while (j < 80 * 25 * 2) {
@@ -16,4 +26,58 @@ void kmain(void)
     i = i + 2;
   }
   return ;
+} 
+void keyboard_handler_main(void) {
+  unsigned char status;
+  char keycode;
+  write_port(0x20, 0x20);
+  status = read_port(0x64);
+  if (status & 0x01) {
+    keycode = read_port(0x60);
+    if(keycode<0) {
+    return;
+  }
+    vidptr[current_loc++] = keyboard_map[keycode];
+    vidptr[current_loc++] = 0x07;
+  }
+}
+
+struct IDT_entry {
+  unsigned short offset_lowerbits;
+  unsigned short selector;
+  unsigned char zero;
+  unsigned char type_attr;
+  unsigned short offset_higherbits;
+};
+
+struct IDT_entry IDT[256];
+
+void idt_init(void){
+  unsigned long keyboard_address;
+  unsigned long idt_address;
+  unsigned long idt_ptr[2];
+  keyboard_address = (unsigned long)keyboard_handler;
+  IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
+  IDT[0x21].selector = 0x08;
+  IDT[0x21].zero = 0;
+  IDT[0x21].type_attr = 0x8e;
+  IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  write_port(0x20, 0x11);
+  idt_address = (unsigned long)IDT;
+  idt_ptr[0] = (sizeof (struct IDT_entry) * 256) + ((idt_address & 0xffff) << 16);
+  idt_ptr[1] = idt_address >> 16;
+  load_idt(idt_ptr);
+}
+
+void kb_init(void) {
+  write_port(0x21,0xFD);
 }
